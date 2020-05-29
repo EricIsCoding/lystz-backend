210f35e3898861fc2784242217e465fe7f3e1316
Eric J Wilhite <EricJWilhite@gmail.com>
2020-05-29 10:37:39 -0700

    remove /api scoping

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  scope '/api' do
     resources :items
     resources :blocks
     resources :vendors
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
